28f90a2174c7a2c386c0d1cf675f0e1848bf4777
Olof2008 <olof.bergsten@elev.ga.ntig.se>
2026-08-25 10:35:38 +0200

    2bilar

---
+++ prog2-klasser.py
@@ -0,0 +1,13 @@
+class Persson:
+    def __init_(self):
+        self.förnamn= ""
+        self.efternamn= ""
+        self.födelseår= ""
+        self.singel= True
+
+person1 = Persson()
+person1.förnamn = "Olof"
+person1.efternamnnamn="Bergsten"
+person1.födelseår="2008"
+
+print(person1.förnamn)
