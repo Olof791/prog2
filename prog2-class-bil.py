361a9aaaf6b9e588ed2aab75e385ac95457a3a67
Olof2008 <olof.bergsten@elev.ga.ntig.se>
2026-08-27 10:39:35 +0200

    förbättrat bil

--- prog2-class-bil.py
+++ prog2-class-bil.py
@@ -1,38 +1,26 @@
 class Bil:
-    def __init__(self):
-        self.ägare= ''
-        self.reg = ""
-        self.märke = ""
-        self.tjänstevikt= ""
-        self.hp = ""
-        self.nm =""
+    def __init__(self, ägare='', reg='', märke='', tjänstevikt='', hp='', nm=''):
+        self.ägare= ägare
+        self.reg = reg
+        self.märke = märke
+        self.tjänstevikt= tjänstevikt
+        self.hp = hp
+        self.nm = nm
 
 class Persson:
-    def __init__(self):
-        self.förnamn = ""
+    def __init__(self, förnamn=''):
+        self.förnamn = förnamn
 
 k = Bil()
-k.ägare = Persson()
-k.ägare.förnamn= 'Bertil'
+k.ägare = Persson(förnamn='Bertil')
 
-bil1 = Bil()
 
-bil1.reg= "SWA199"
-bil1.märke= "Bmw"
-bil1.tjänstevikt="1395kg"
-bil1.hp="193hp"
-bil1.nm="280nm"
+bil1 = Bil(ägare=Persson(förnamn='Bertil'), reg="SWA199", märke="Bmw", tjänstevikt="1395kg", hp="193hp", nm="280nm")
 
 
 
 
-bil2 = Bil()
-
-bil2.reg="xlp856"
-bil2.märke="Bmw"
-bil2.tjänstevikt="1545kg"
-bil2.hp="286hp"
-bil2.nm="580nm"
+bil2 = Bil(ägare=Persson(förnamn='Gustaf'), reg="xlp856", märke="Bmw", tjänstevikt="1545kg", hp="286hp", nm="580nm")
 
 print(bil1.reg, bil1.märke, bil1.tjänstevikt, bil1.hp, bil1.nm)
 print(bil2.reg, bil2.märke, bil2.tjänstevikt, bil2.hp, bil2.nm)
